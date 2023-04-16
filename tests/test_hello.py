ec57154ffa39f8123389a06942b81e269d517b34
Eugene Romancenko <eugene@romancenko.com>
2023-04-16 15:55:00 +0100

    new app version

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -9,4 +9,4 @@ def test_main_page(client):
     Test the Hello World endpoint of the app.
     """
     response = client.get('/')
-    assert response.data == b'Hello, World'
+    assert response.data == b'Hello, World Ver1'
