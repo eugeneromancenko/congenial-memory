b0256b65703b8bc7b2cc96e9537a0ddb0c31d19b
Eugene Romancenko <eugene@romancenko.com>
2023-04-16 13:47:24 +0100

    fixing some pylint errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,23 @@
-import pytest 
+"""
+This module defines fixtures and configuration for tests.
+"""
+
+import pytest
 
 from hello import create_app
 
 @pytest.fixture
 def app():
+    """
+    Create an instance of the Hello World app for testing.
+    """
     app = create_app()
     yield app
 
 @pytest.fixture
 def client(app):
-    return app.test_client()
+    """
+    Create a test client for the Hello World app..
+    """
+    test_client = app.test_client()
+    return test_client()
